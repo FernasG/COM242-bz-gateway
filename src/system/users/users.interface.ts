6c0398696a05c8cb9c3f3e382b595f6dced3abbf
FernasG <fernandogoul7@gmail.com>
2023-11-13 00:23:46 -0300

    refactor: remove useless type from users module

--- src/system/users/users.interface.ts
+++ src/system/users/users.interface.ts
@@ -3,8 +3,3 @@ import { PartialType } from '@nestjs/mapped-types';
 export class CreateUserDto { }
 
 export class UpdateUserDto extends PartialType(CreateUserDto) { }
-
-export class SignInDto {
-  email: string;
-  password: string;
-}
